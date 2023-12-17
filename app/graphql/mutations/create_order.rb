d57962a46aa81110a61a73cd4b82cdba5366bd71
kwame bio <kwameagyemang73@gmail.com>
2023-12-17 22:18:54 +0000

    fix order feature

--- app/graphql/mutations/create_order.rb
+++ app/graphql/mutations/create_order.rb
@@ -8,25 +8,7 @@ module Mutations
         field :errors, [String], null: false
 
         def resolve(params)
-            user = User.find_by(id: params[:user_id])
-            return {order: nil, errors: ["User not found"]} unless user
-
-            product = Product.find_by(id: params[:product_id])
-            return {order: nil, errors: ["Product not found"]} unless product
-
-            order = Order.new(params.to_h)
-            order.build_placements_with_product_ids_and_quantities
-            if order.save
-                {
-                    order: order,
-                    errors: []
-                }
-            else
-                {
-                    order: nil,
-                    errors: order.errors.full_messages
-                }
-            end
+         #fix this
         end
     end
 end
