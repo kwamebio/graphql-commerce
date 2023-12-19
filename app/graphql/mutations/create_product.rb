6bdae8a0097dd059754d6086e09786ef24d9d11f
kwame bio <kwameagyemang73@gmail.com>
2023-12-19 01:41:37 +0000

    clean up

--- app/graphql/mutations/create_product.rb
+++ app/graphql/mutations/create_product.rb
@@ -3,27 +3,24 @@ module Mutations
         argument :name, String, required: true
         argument :description, String, required: true
         argument :price, Float, required: true
-        argument :user_id, ID, required: true   
+          
 
         field :product, Types::ProductType, null: false
         field :errors, [String], null: false
 
-        def resolve(params)
-           user = User.find_by(id: params[:user_id])
-           return {product: nil, errors: ["User not found"]} unless user
-
-           product = Product.new(params.to_h)
-           if product.save
-               {
-                   product: product,
-                   errors: []
-               }
-           else
-               {
-                   product: nil,
-                   errors: product.errors.full_messages
-               }
-           end
+        def resolve(name:, description:, price:)
+            product = Product.new(name: name, description: description, price: price, user: current_user)
+            if product.save
+                {
+                    product: product,
+                    errors: []
+                }
+            else
+                {
+                    product: nil,
+                    errors: product.errors.full_messages
+                }
+            end
         end
 
 
